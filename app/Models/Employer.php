87255eec2b482c8c216c8743905eebf2d2442089
Japoivvb <jose.portugal@stucom.com>
2025-02-16 23:48:16 +0100

    day10 model Factories

---
+++ app/Models/Employer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employer extends Model
+{
+    //
+    use HasFactory;// to use factory in order to add data
+}
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+
+    use HasFactory;// to use factory in order to add data
     
     protected $table = 'job_listings';// to link model with table
     protected $fillable = ['name','email','title','salary'];// to list attributes allowed to be insert/update
