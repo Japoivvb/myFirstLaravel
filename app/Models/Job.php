b97cef85f75ced9c4ea3dc9b0399743038f5535e
Japoivvb <jose.portugal@stucom.com>
2025-02-16 23:10:10 +0100

    day9 model Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,58 +2,13 @@
 
 namespace App\Models;
 
-class Job
-{
-    public $id;
-    public $name;
-    public $email;
-    public $title;
-    public $salary;
+use Illuminate\Database\Eloquent\Model;
 
-    public function __construct($id, $name, $email, $title, $salary)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
-        $this->title = $title;
-        $this->salary = $salary;
-    }
+class Job extends Model {
+    
+    protected $table = 'job_listings';// to link model with table
+    protected $fillable = ['name','email','title','salary'];// to list attributes allowed to be insert/update
 
-    public static function find(int $id): array{
-        $job = collect(static::all())->firstWhere('id', $id);
-
-        if(!$job){
-            abort(404);// handle unexpected values
-        }
-        return $job;
-    }
-
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'name' => 'John Doe',
-                'email' => 'john@gmail.com',
-                'title' => 'director',
-                'salary' => 10000
-            ],
-            [
-                'id' => 2,
-                'name' => 'Lucy Lee',
-                'email' => 'lucy@gmail.com',
-                'title' => 'president',
-                'salary' => 20000
-            ],
-            [
-                'id' => 3,
-                'name' => 'Ben Tex',
-                'email' => 'ben@gmail.com',
-                'title' => 'manager',
-                'salary' => 30000
-            ],
-        ];
-    }
-
+    
     
 }
